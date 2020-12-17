7755630ff6285fa040beeb41203bd552ad670183
abpuccini <b.atcharaporn@gmail.com>
2020-12-17 11:32:46 -0500

    Gauge Chart

--- static/js/app.js
+++ static/js/app.js
@@ -109,7 +109,7 @@ function demoGraphic(id) {
             var keys = capitalizeFirstLetter(key);
         }
         var info = demo_box.append("p")
-        info.text(`${keys}: ${value}`);
+        info.text(`${keys}` + ": " + `${value}`);
     });
 }
 
@@ -133,8 +133,6 @@ function bubbleChart(id) {
         colors.push(color);
     };
 
-    console.log(color);
-
     // trace for bubble chart
     var trace = {
         x: otu_ids,
@@ -143,6 +141,7 @@ function bubbleChart(id) {
         marker: {
             size: sample_values,
             color: colors,
+            colorscale: 'Earth',
             opacity: otu_ids.map(id => 0.7)
         },
         type: 'scatter',
@@ -161,40 +160,90 @@ function bubbleChart(id) {
     Plotly.newPlot('bubble', data, layout);
 }
 
+
 function gaugeChart(value) {
     var filteredData = metadata.filter(event => parseInt(event.id) === parseInt(value))[0];
     var wfreq = filteredData.wfreq;
+
+    // Source: https://codepen.io/ascotto/pen/eGNaqe?editors=0010
+    // Trig to calc meter point
+    function gaugePointer(value) {
+
+        var degrees = 180 - (value * 20),
+            radius = .5;
+        var radians = degrees * Math.PI / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
+
+        // Path: may have to change to create a better triangle
+        var mainPath = 'M -.0 -0.035 L .0 0.035 L ',
+            pathX = String(x),
+            space = ' ',
+            pathY = String(y),
+            pathEnd = ' Z';
+        var path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+        return path;
+    }
+
     var data = [
         {
-            domain: { x: [0, 1], y: [0, 1] },
             value: wfreq,
-            type: "indicator",
-            mode: "gauge+number",
-            text: ['0-1', '1-2', '2-3', '3-4',
-                '4-5', '5-6', '6-7', '7-8', '8-9'],
             gauge: {
-                axis: { range: [null, 9] },
-                steps: [
-                    { range: [0, 1], color: "#f0f5e6" },
-                    { range: [1, 2], color: "#e0ebcc" },
-                    { range: [2, 3], color: "#c9dba6" },
-                    { range: [3, 4], color: "#bad18c" },
-                    { range: [4, 5], color: "#80b280" },
-                    { range: [5, 6], color: "#66a366" },
-                    { range: [6, 7], color: "#597a59" },
-                    { range: [7, 8], color: "gray" },
-                    { range: [8, 9], color: "gray" },
-                ],
-
-            }
-        }
-    ];
+                axis: { range: [null, 9] }
+            },
+            type: 'scatter',
+            x: [0], y: [0],
+            marker: { size: 18, color: '850000' },
+            showlegend: false,
+            name: 'wfreq',
+            text: wfreq,
+            hoverinfo: 'text+name'
+        },
+        {
+            values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+            rotation: 90,
+            text: ['8-9', '7-8', '6-7', '5-6',
+                '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+            textinfo: 'text',
+            textposition: 'inside',
+            marker: {
+                colors: ['rgba(0, 25, 0, .5)', 'rgba(5, 50, 22, .5)',
+                    'rgba(10, 90, 42, .5)', 'rgba(14, 127, 0, .5)',
+                    'rgba(110, 154, 22, .5)', 'rgba(170, 202, 42, .5)',
+                    'rgba(202, 209, 95, .5)', 'rgba(210, 206, 145, .5)',
+                    'rgba(232, 226, 202, .5)', 'rgba(255, 255, 255, 0)']
+            },
+            labels: ['8-9', '7-8', '6-7', '5-6',
+                '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+            hoverinfo: 'label',
+            hole: .5,
+            type: 'pie',
+            showlegend: false
+        }];
 
     var layout = {
-
+        shapes: [{
+            type: 'path',
+            path: gaugePointer(wfreq),
+            fillcolor: '850000',
+            line: {
+                color: '850000'
+            }
+        }],
         margin: { t: 100, b: 0 },
         title: { text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week" },
         autosize: true,
+        xaxis: {
+            zeroline: false, showticklabels: false,
+            showgrid: false, range: [-1, 1]
+        },
+        yaxis: {
+            zeroline: false, showticklabels: false,
+            showgrid: false, range: [-1, 1]
+        }
     };
+
     Plotly.newPlot('gauge', data, layout);
 };
+
